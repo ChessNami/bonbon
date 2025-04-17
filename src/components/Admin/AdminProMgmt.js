a17192bd2dba9131a6d35c6ae0a2301d25b481b2
ChessNami <betita.jessamaeobsioma@gmail.com>
2025-04-17 10:45:18 +0800

    Add Project Management component and update AdminMainPage and AdminSidebar

---
+++ src/components/Admin/AdminProMgmt.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminProMgmt = () => {
+    return (
+        <div>
+            <h1>Admin Product Management</h1>
+            <p>This is the Admin Product Management component.</p>
+        </div>
+    );
+};
+
+export default AdminProMgmt;
