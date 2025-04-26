22282f7d9eb2d958dd64db4630582080a6524391
ChessNami <betita.jessamaeobsioma@gmail.com>
2025-04-27 00:48:04 +0800

    feat: Add tabIndex to password visibility toggle buttons for improved accessibility

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -368,6 +368,7 @@ const Auth = ({ onLoginSuccess }) => {
                                                 type="button"
                                                 className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700 touch-auto"
                                                 onClick={() => setShowPassword(!showPassword)}
+                                                tabIndex={-1}
                                             >
                                                 {showPassword ? <FaEye /> : <FaEyeSlash />}
                                             </button>
@@ -393,6 +394,7 @@ const Auth = ({ onLoginSuccess }) => {
                                                     type="button"
                                                     className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700 touch-auto"
                                                     onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                                                    tabIndex={-1}
                                                 >
                                                     {showConfirmPassword ? <FaEye /> : <FaEyeSlash />}
                                                 </button>
